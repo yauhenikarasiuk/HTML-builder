5606f1fbb856aeaefe432acdb5db1375f4868f53
Yauheni Karasiuk <yauheni.karasiuk@gmail.com>
2022-05-18 23:00:03 +0300

    feat: solve task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+let fileData = '';
+fs.writeFile(path.join(__dirname, 'text.txt'), fileData, () => {});
+console.log('Please, write some text end press enter. If you want to exit type \'exit\'');
+stdin.on('data', (data) => {
+  if(data.toString() == 'exit\n'){
+    process.exit();
+  } else {
+    fileData += data.toString();
+    fs.writeFile(path.join(__dirname, 'text.txt'), fileData, () => {});
+  }
+});
+process.on('SIGINT', ()=>{
+  process.exit();
+});
+process.on('exit', ()=>{
+  console.log('\nBye Bye');
+});
