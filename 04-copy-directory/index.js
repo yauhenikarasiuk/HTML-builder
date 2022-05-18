73162a8ddf090b0ee9635d92fd160872e4933c18
Yauheni Karasiuk <yauheni.karasiuk@gmail.com>
2022-05-19 00:07:58 +0300

    feat: solve task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
-
+const fs = require('fs');
+const path = require('path');
+const copyPathName = path.resolve(__dirname, 'files-copy');
+const origPathName = path.resolve(__dirname, 'files');
+fs.mkdir(copyPathName, (error) => {
+  if(error){
+    return;
+  }
+  fs.readdir(origPathName, (error, files) => {
+    files.forEach(file => {
+      const origFilePath = path.resolve(origPathName, file.toString());
+      const copyFilePath = path.resolve(copyPathName, file.toString());
+      fs.copyFile(origFilePath, copyFilePath, ()=>{});
+    });
+  });
+});
