bbb5d4d191a50588190eb99d913b20488fbfc882
Yauheni Karasiuk <yauheni.karasiuk@gmail.com>
2022-05-20 00:20:51 +0300

    fix: fix task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const copyPathName = path.resolve(__dirname, 'files-copy');
 const origPathName = path.resolve(__dirname, 'files');
-fs.mkdir(copyPathName, (error) => {
-  if(error){
-    return;
-  }
+fs.mkdir(copyPathName, () => {
   fs.readdir(origPathName, (error, files) => {
     files.forEach(file => {
       const origFilePath = path.resolve(origPathName, file.toString());
