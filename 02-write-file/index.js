e9e4ad2d73df516dc7ed1dad123ab11e8588d825
Yauheni Karasiuk <yauheni.karasiuk@gmail.com>
2022-05-21 00:15:21 +0300

    fix: add exit logic for windows users

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,11 @@
-const { stdin, stdout } = process;
+const { stdin } = process;
 const fs = require('fs');
 const path = require('path');
 let fileData = '';
 fs.writeFile(path.join(__dirname, 'text.txt'), fileData, () => {});
 console.log('Please, write some text end press enter. If you want to exit type \'exit\'');
 stdin.on('data', (data) => {
-  if(data.toString() == 'exit\n'){
+  if(data.toString() == 'exit\n' || data.toString() == 'exit\r\n'){
     process.exit();
   } else {
     fileData += data.toString();
